ddb7a95412dbf747f9db57b4e3b2615737bae867
Pavithra <pavithraannadurai2@gmail.com>
2025-04-15 15:02:02 +0530

    url change

--- frontend/src/Services/Api.js
+++ frontend/src/Services/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const API = axios.create({baseURL:'mern-note-keeper.vercel.app'});
-const API = axios.create({baseURL:'http://localhost:5000'});
+const API = axios.create({baseURL:'https://mern-notekeeper-5at7.onrender.com'});
 
 API.interceptors.request.use((req) => {
     const token = localStorage.getItem('token');
